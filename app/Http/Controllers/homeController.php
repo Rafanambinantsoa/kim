443605aaf9c712eeea4190b5eb9a56ef3ce44600
Rafanambinantsoa <akutagawakarim@gmail.com>
2023-04-14 14:40:07 +0300

    FIrst commit rien de plus

---
+++ app/Http/Controllers/homeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class homeController extends Controller
+{
+    public function homePage(){
+        return view('homePage');
+    }
+
+    public function aboutPage(){
+        return view('single-post');
+    }
+}
